3f00341bc2191dc23f58ea3c4247ecca2a99396f
Suryansh <suryansh.anand@icloud.com>
2024-09-25 18:52:09 -0400

    removed ctx

--- main.go
+++ main.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-    "context"
-
     "github.com/gin-gonic/gin"
 
-    db "appointbuzz/api/v1/lib"
-    redis "appointbuzz/api/v1/lib"
+    lib "appointbuzz/api/v1/lib"
     v1 "appointbuzz/api"
 )
 
-var ctx = context.Background()
-
 func main() {
-    redis.InitRedis()
+    lib.InitRedis()
 
-    db.InitializeDatabase()
+    lib.InitializeDatabase()
 
     router := gin.Default()
     v1.SetupRouter(router)
